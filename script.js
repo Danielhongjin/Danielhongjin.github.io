a5ca5b73835a37c9a6df8fdb7ad2f7160eabe1db
DanielHongJin <Danielhongjin@gmail.com>
2020-05-01 00:48:24 -0700

    second commit

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ if (!location.hash) {
 }
 const roomHash = location.hash.substring(1);
 
-const drone = new ScaleDrone('BIZhUxYEmI9Hwh9I');
+// TODO: Replace with your own channel ID
+const drone = new ScaleDrone('yiS12Ts5RdNhebyM');
+// Room name needs to be prefixed with 'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
   iceServers: [{
@@ -13,7 +15,7 @@ const configuration = {
 };
 let room;
 let pc;
-let dataChannel;
+
 
 function onSuccess() {};
 function onError(error) {
@@ -26,16 +28,21 @@ drone.on('open', error => {
   }
   room = drone.subscribe(roomName);
   room.on('open', error => {
-    if (error) { onError(error);}
+    if (error) {
+      onError(error);
+    }
   });
-
+  // We're connected to the room and received an array of 'members'
+  // connected to the room (including us). Signaling server is ready.
   room.on('members', members => {
     console.log('MEMBERS', members);
+    // If we are the second user to connect to the room we will be creating the offer
     const isOfferer = members.length === 2;
     startWebRTC(isOfferer);
   });
 });
 
+// Send signaling data via Scaledrone
 function sendMessage(message) {
   drone.publish({
     room: roomName,
@@ -43,99 +50,59 @@ function sendMessage(message) {
   });
 }
 
-
-
-function checkDataChannelState() {
-  console.log('WebRTC channel state is:', dataChannel.readyState);
-  if (dataChannel.readyState === 'open') {
-    insertMessageToDOM({content: 'WebRTC data channel is now open'});
-  }
-}
-
-function setupDataChannel() {
-  checkDataChannelState();
-  dataChannel.onopen = checkDataChannelState;
-  dataChannel.onclose = checkDataChannelState;
-  dataChannel.onmessage = event => insertMessageToDom(JSON.parse(event.data), false)
-}
-
-function startListeningToSignals() {
-  room.on('data', (message, client) => {
-    if (client.id === drone.clientId) {
-      return;
-    }
-    if (message.sdp) {
-      pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
-        console.log('pc.remoteDescription.type', pc.remoteDescription.type);
-        if (pc.remoteDescription.type === 'offer') {
-          console.log('Answering offer');
-          pc.createAnswer(localDescCreated, error => console.error(error));
-        }
-      }, error => console.error(error));
-    } else if (message.candidate) {
-      pc.addIceCandidate(newRTCIceCandidate(message.candidate));
-    }
-  });
-}
-
-function localDescCreated(desc) {
-  pc.setLocalDescription(desc, () => sendSignalingMessage({'sdp': pc.localDescription}), error => console.error(error));
-}
-
-
-//The functions that actually instantiate a video chat.
 function startWebRTC(isOfferer) {
-
   pc = new RTCPeerConnection(configuration);
+
+  // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
+  // message to the other peer through the signaling server
   pc.onicecandidate = event => {
     if (event.candidate) {
       sendMessage({'candidate': event.candidate});
     }
   };
 
+  // If user is offerer let the 'negotiationneeded' event create the offer
   if (isOfferer) {
     pc.onnegotiationneeded = () => {
       pc.createOffer().then(localDescCreated).catch(onError);
     }
-    dataChannel = pc.createDataChannel('chat');
-    
-    setupDataChannel();
-  } else {
-    pc.ondatachannel = event => {
-      dataChannel = event.channel;
-      setupDataChannel();
-    }
   }
 
-  
+  // When a remote stream arrives display it in the #remoteVideo element
   pc.ontrack = event => {
     const stream = event.streams[0];
     if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
       remoteVideo.srcObject = stream;
     }
-    startListeningToSignals();
   };
-  
-  //Links the received media to the video elements
+
   navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   }).then(stream => {
+    // Display your local video in #localVideo element
     localVideo.srcObject = stream;
+    // Add your stream to be sent to the conneting peer
     stream.getTracks().forEach(track => pc.addTrack(track, stream));
   }, onError);
 
+  // Listen to signaling data from Scaledrone
   room.on('data', (message, client) => {
+    // Message was sent by us
     if (client.id === drone.clientId) {
       return;
     }
+
     if (message.sdp) {
+      // This is called after receiving an offer or answer from another peer
       pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+        // When receiving an offer lets answer it
         if (pc.remoteDescription.type === 'offer') {
           pc.createAnswer().then(localDescCreated).catch(onError);
         }
       }, onError);
     } else if (message.candidate) {
+      // Add the new ICE candidate to our connections remote description
       pc.addIceCandidate(
         new RTCIceCandidate(message.candidate), onSuccess, onError
       );
@@ -149,42 +116,3 @@ function localDescCreated(desc) {
     () => sendMessage({'sdp': pc.localDescription}),
     onError
   );
-}
-
-function insertMessageToDOM(options, isFromMe) {
-  const template = document.querySelector('template[data-template="message"]');
-  const nameEl = template.content.querySelector('.message__name');
-  template.content.querySelector('.message__bubble').innerText = options.content;
-  const clone = document.importNode(template.content, true);
-  const messageEl = clone.querySelector('.message');
-  if (isFromMe) {
-    messageEl.classList.add('message--mine');
-  } else {
-    messageEl.classList.add('message--theirs');
-  }
- 
-  const messagesEl = document.querySelector('.messages');
-  messagesEl.appendChild(clone);
- 
-  // Scroll to bottom
-  messagesEl.scrollTop = messagesEl.scrollHeight - messagesEl.clientHeight;
-}
-
-
-const form = document.querySelector('form');
-form.addEventListener('submit', () => {
-  const input = document.querySelector('input[type="text"]');
-  const value = input.value;
-  input.value = '';
- 
-  const data = {
-    name,
-    content: value,
-  };
- 
-  dataChannel.send(JSON.stringify(data));
- 
-  insertMessageToDOM(data, true);
-});
- 
-insertMessageToDOM({content: 'Chat URL is ' + location.href});
